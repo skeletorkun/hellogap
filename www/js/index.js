e83312bbe8ebd4ed8bc7f87f4ead288d78e33283
unknown <oozen@NCED850.iis.amadeus.net>
2014-03-03 16:28:50 +0100

    test

--- www/js/index.js
+++ www/js/index.js
@@ -40,10 +40,9 @@ var app = {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
-
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
+        
         console.log('Received Event: ' + id);
     }
 };
